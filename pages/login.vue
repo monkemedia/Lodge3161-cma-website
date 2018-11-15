e5f3a37fd2ec6301bcf99c618616751b146d834d
monkemedia <info@monkemedia.co.uk>
2018-11-15 19:06:16 +0000

    First commit

---
+++ pages/login.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  #top(v-cloak)
+    p hello
+
+</template>
+
+<script>
+  // import HeaderHomepage from '@/components/Headers/HeaderHomepage.vue'
+
+  export default {
+    components: {
+    },
+
+    async fetch ({ store }) {
+    },
+
+    mounted () {
+      if (!process.client) return
+
+      // return this.$store.dispatch('homepage/fetchData')
+    },
+
+    computed: {
+      homepage () {
+        return this.$store.getters['homepage/getData']
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
