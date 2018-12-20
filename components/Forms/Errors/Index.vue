bd5b26ae4ef1ecac42bc109faec7c8e681461f7d
monkemedia <info@monkemedia.co.uk>
2018-12-20 18:04:33 +0000

    Error message is now in a component

---
+++ components/Forms/Errors/Index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .notification.is-danger(
+    v-if="errorMessage" 
+    :class="{ 'is-error': errorMessage }")
+    p {{ errorMessage }}
+</template>
+
+<script>
+  export default {
+    props: {
+      errorMessage: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- plugins/form-components.js
+++ plugins/form-components.js
@@ -3,8 +3,10 @@ import inputField from '@/components/Forms/Fields/Input'
 import textareaField from '@/components/Forms/Fields/Textarea'
 import checkboxField from '@/components/Forms/Fields/Checkbox'
 import markdownTextareaField from '@/components/Forms/Fields/MarkdownTextarea'
+import errorMessage from '@/components/Forms/Errors/Index'
 
 Vue.component('inputField', inputField)
 Vue.component('textareaField', textareaField)
 Vue.component('checkboxField', checkboxField)
 Vue.component('markdownTextareaField', markdownTextareaField)
+Vue.component('errorMessage', errorMessage)
